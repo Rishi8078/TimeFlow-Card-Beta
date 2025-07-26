7801b4d8d0475178b8ec0527b1e5cdf46b7415c3
Rishib Rajeev <rishibrajeev1999@gmail.com>
2025-07-26 23:50:26 +0200

    fixed getstubconfig

--- src/components/TimeFlowCard.ts
+++ src/components/TimeFlowCard.ts
@@ -134,7 +134,8 @@ export class TimeFlowCardBeta extends LitElement {
   static getStubConfig(): CardConfig {
     return {
       type: 'custom:timeflow-card-beta',
-      target_date: '2026-12-31T23:59:59',
+      target_date: '2025-12-31T23:59:59',
+      creation_date: '2024-12-31T23:59:59',
       title: 'New Year Countdown',
       show_days: true,
       show_hours: true,
